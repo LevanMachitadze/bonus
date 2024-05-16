b326740d07142151f434de3b0affe406a2fad186
LevanMachitadze <160760563+LevanMachitadze@users.noreply.github.com>
2024-05-16 22:15:16 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function task() {
+  let firstName = prompt('enter your first name');
+  let secondName = prompt('enter your second name');
+  alert(`${firstName[0].toUpperCase()}. ${secondName[0].toUpperCase()}.`);
+}
+task();
